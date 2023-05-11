336053e1332b6ab1fb74889114f6d9057a7e0f2c
Ríona Stokes <rionastokes@gmail.com>
2023-05-11 18:14:43 +0200

    Script progress

--- plugin-admin/create_bak_include.js
+++ plugin-admin/create_bak_include.js
@@ -1,12 +1,22 @@
 // Create empty object structure
 // Read plugins.ejs object in here and save as local variable
-// For each dep in node_modules (as we don't want the ones not on npm):
+// For each dep in package.json (as we don't want the ones not on npm):
+//   NOTE: scoped packages under node_modules/@seneca/title, unscoped under node_modules/name
 //   Write relevant information from package.json (stored in node_modules folder) to object
 //   Write relevant information not found in package.json from plugins.ejs variable to object
-//   Sidenote: how to get groups if not every plugin.ejs is going in?
+//   NOTE: how to get groups if not every plugin.ejs is going in?
 // End for
 // Convert data to string to send
 // Write string data to file in /plugins-2023 to be included in another file in that dir
+
+let pluginData = {}
+
+let pkgjson = require('./package.json')
+Object.keys(pkgjson.dependencies).forEach((dep) => {
+  let path = './node_modules/' + dep
+  let pluginPkg = require(path)
+  console.log(dep + pluginPkg.description)
+})
 
 // Group name: ejs
 // Group desc: ejs
