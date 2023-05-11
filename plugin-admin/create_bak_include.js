6de7ed15c60c61a4296598ee874597426dad63df
Ríona Stokes <rionastokes@gmail.com>
2023-05-11 17:40:35 +0200

    Script algo

--- plugin-admin/create_bak_include.js
+++ plugin-admin/create_bak_include.js
@@ -1,2 +1,11 @@
-// Write to file
+// Write to file (ex)
 // const src = `<% const pkgdata = ${JSON.stringify(allTheBits)} %>`
+
+// Create empty object structure
+// Read plugins.ejs object in here and save as local variable
+// For each dep in node_modules (as we don't want the ones not on npm):
+//   Write relevant information from package.json (stored in node_modules folder) to object
+//   Write relevant information not found in package.json from plugins.ejs variable to object
+// End for
+// Convert data to string to send
+// Write string data to file in /plugins-2023 to be included in another file in that dir
