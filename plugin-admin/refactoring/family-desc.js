3028c0fc79ddbca3327cc4fecbe9c5077c55604e
Ríona Stokes <rionastokes@gmail.com>
2023-05-24 19:24:09 +0200

    Creation of file for editing family descriptions and members (plugins associated)

---
+++ plugin-admin/refactoring/family-desc.js
@@ -0,0 +1,85 @@
+module.exports = {
+  // Only contains families published on npm
+  families: {
+    cache: {
+      short_desc: '',
+      long_desc: '',
+      plugins: [],
+    },
+    entity: {
+      short_desc: '',
+      long_desc: '',
+      plugins: [],
+    },
+    gateway: {
+      short_desc: '',
+      long_desc: '',
+      plugins: [],
+    },
+    msg: {
+      short_desc: '',
+      long_desc: '',
+      plugins: [],
+    },
+    redis: {
+      short_desc: '',
+      long_desc: '',
+      plugins: [],
+    },
+    search: {
+      short_desc: '',
+      long_desc: '',
+      plugins: [],
+    },
+    telemetry: {
+      short_desc: '',
+      long_desc: '',
+      plugins: [],
+    },
+    transport: {
+      short_desc: '',
+      long_desc: '',
+      plugins: [],
+    },
+    user: {
+      short_desc: '',
+      long_desc: '',
+      plugins: [],
+    },
+    web: {
+      short_desc: '',
+      long_desc: '',
+      plugins: [],
+    },
+    logger: {
+      short_desc: '',
+      long_desc: '',
+      plugins: [],
+    },
+    provider: {
+      short_desc: '',
+      long_desc: '',
+      plugins: [],
+    },
+    store: {
+      short_desc: '',
+      long_desc: '',
+      plugins: [],
+    },
+    test: {
+      short_desc: '',
+      long_desc: '',
+      plugins: [],
+    },
+    registry: {
+      short_desc: '',
+      long_desc: '',
+      plugins: [],
+    },
+    various: {
+      short_desc: '',
+      long_desc: '',
+      plugins: [],
+    },
+  },
+}
